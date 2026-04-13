af2790b50764692348838e7b07f671150ba56118
Erik Neves <erikpdn1@gmail.com>
2026-04-12 23:24:58 -0300

    feat: add dynamic background gradient to player screen using artwork colors

--- src/app/player.tsx
+++ src/app/player.tsx
@@ -3,11 +3,13 @@ import { RepeatButton } from '@/components/RepeatButton'
 import SeekBar from '@/components/SeekBar'
 import { ShuffleButton } from '@/components/ShuffleButton'
 import { SkipNextButton, SkipPreviousButton } from '@/components/SkipButton'
-import { unknownArtistImageSource } from '@/constants/images'
+import { unknownArtistImageSource, unknownTrackImageSource } from '@/constants/images'
 import { colors, screenPadding } from '@/constants/tokens'
+import { usePlayerBackground } from '@/hooks/usePlayerBackground'
 import { defaultStyles } from '@/styles'
 import { FontAwesome, FontAwesome6 } from '@expo/vector-icons'
 import { Image } from 'expo-image'
+import { LinearGradient } from 'expo-linear-gradient'
 import { useRouter } from 'expo-router'
 import { useState } from 'react'
 import { ActivityIndicator, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
@@ -18,6 +20,8 @@ const PlayerScreen = () => {
 	const router = useRouter()
 	const activeTrack = useActiveTrack()
 	const { top, bottom } = useSafeAreaInsets()
+	const { imageColors } = usePlayerBackground(activeTrack?.artwork ?? unknownTrackImageSource)
+	console.log('Image colors: ', imageColors)
 
 	// TODO: Implement actual like functionality
 	const [isLiked, setIsLiked] = useState(false)
@@ -35,76 +39,84 @@ const PlayerScreen = () => {
 	}
 
 	return (
-		<View style={styles.overlayContainer}>
-			<FontAwesome6 name="chevron-down" size={24} color="white" onPress={handleOnPress} />
-
-			<View
-				style={[
-					styles.cardContainer,
-					{
-						marginTop: top + 10,
-						marginBottom: bottom,
-					},
-				]}
-			>
-				<View style={styles.artworkImageContainer}>
-					<Image
-						source={activeTrack.artwork ?? unknownArtistImageSource}
-						style={styles.artworkImage}
-					/>
-				</View>
+		<LinearGradient
+			style={{ flex: 1 }}
+			colors={
+				imageColors
+					? [imageColors.average ?? colors.background, imageColors.darkMuted ?? colors.background]
+					: [colors.background, colors.background]
+			}
+		>
+			<View style={styles.overlayContainer}>
+				<FontAwesome6 name="chevron-down" size={24} color="white" onPress={handleOnPress} />
 
 				<View
-					style={{
-						marginTop: 12,
-						flexDirection: 'row',
-						justifyContent: 'space-between',
-						alignItems: 'center',
-					}}
+					style={[
+						styles.cardContainer,
+						{
+							marginTop: top + 10,
+							marginBottom: bottom,
+						},
+					]}
 				>
-					<View style={{ flexDirection: 'column', maxWidth: '75%' }}>
-						<Text numberOfLines={1} style={styles.trackTitleText}>
-							{activeTrack.title}
-						</Text>
-						<Text numberOfLines={1} style={styles.trackArtistText}>
-							{activeTrack.artist || 'Unknown Artist'}
-						</Text>
+					<View style={styles.artworkImageContainer}>
+						<Image
+							source={activeTrack.artwork ?? unknownArtistImageSource}
+							style={styles.artworkImage}
+						/>
 					</View>
 
-					<TouchableOpacity onPress={() => setIsLiked((prev) => !prev)} style={styles.likeButton}>
-						<FontAwesome name="heart" size={26} color={isLiked ? colors.primary : colors.icon} />
-					</TouchableOpacity>
-				</View>
+					<View
+						style={{
+							marginTop: 12,
+							flexDirection: 'row',
+							justifyContent: 'space-between',
+							alignItems: 'center',
+						}}
+					>
+						<View style={{ flexDirection: 'column', maxWidth: '75%' }}>
+							<Text numberOfLines={1} style={styles.trackTitleText}>
+								{activeTrack.title}
+							</Text>
+							<Text numberOfLines={1} style={styles.trackArtistText}>
+								{activeTrack.artist || 'Unknown Artist'}
+							</Text>
+						</View>
 
-				<View style={styles.seekContainer}>
-					<SeekBar />
-				</View>
+						<TouchableOpacity onPress={() => setIsLiked((prev) => !prev)} style={styles.likeButton}>
+							<FontAwesome name="heart" size={26} color={isLiked ? colors.primary : colors.icon} />
+						</TouchableOpacity>
+					</View>
 
-				<View style={styles.trackControlsContainer}>
-					<ShuffleButton iconSize={28} />
+					<View style={styles.seekContainer}>
+						<SeekBar />
+					</View>
 
-					<SkipPreviousButton iconSize={28} />
+					<View style={styles.trackControlsContainer}>
+						<ShuffleButton iconSize={28} />
 
-					<PlayerPlayPauseButton
-						iconSize={26}
-						iconColor="black"
-						style={styles.playPauseButtonContainer}
-					/>
+						<SkipPreviousButton iconSize={28} />
 
-					<SkipNextButton iconSize={28} />
+						<PlayerPlayPauseButton
+							iconSize={26}
+							iconColor="black"
+							style={styles.playPauseButtonContainer}
+						/>
 
-					<RepeatButton iconSize={28} />
+						<SkipNextButton iconSize={28} />
+
+						<RepeatButton iconSize={28} />
+					</View>
 				</View>
 			</View>
-		</View>
+		</LinearGradient>
 	)
 }
 
 const styles = StyleSheet.create({
 	overlayContainer: {
-		...defaultStyles.container,
+		flex: 1,
 		paddingHorizontal: screenPadding.horizontal,
-		backgroundColor: '#252525',
 		paddingTop: 72,
 	},
 
